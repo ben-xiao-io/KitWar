158df57f40829f39b83c8b4c537976e5c4b0bc52
Benoolean <benxiao778@gmail.com>
2020-08-11 23:40:34 -0400

    Fixed join team null exception + cleanup
    reduced the code clutter and fixed several bugs
    
    - todo: implement minigame logistics

--- src/com/benoolean/KitWar/TeamScoreLogic.java
+++ src/com/benoolean/KitWar/TeamScoreLogic.java
@@ -2,10 +2,12 @@ package com.benoolean.KitWar;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.scoreboard.*;
 
 import java.util.HashMap;
@@ -16,10 +18,6 @@ public class TeamScoreLogic implements Listener {
     static class KitTeam {
         String name;
         ChatColor teamColor;
-        int score;
-        int totalKillCount;
-        int totalDeathCount;
-        Player mvp;
 
         public KitTeam (String name, ChatColor teamColor) {
             this.teamColor = teamColor;
@@ -34,62 +32,50 @@ public class TeamScoreLogic implements Listener {
 
     public static Scoreboard scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
     public static Objective objective = scoreboard.registerNewObjective("Kit War", "dummy", "Player Info");
-    public static Team teamRed = scoreboard.registerNewTeam("Red");
-    public static Team teamBlue = scoreboard.registerNewTeam("Blue");
-    public static Team teamNone = scoreboard.registerNewTeam("No Team");
+    public static HashMap<String, Team> ScoreboardTeamMap = new HashMap<>();
 
     public TeamScoreLogic() {
         KitWar plugin = KitWar.getInstance();
 
+        // team init
+        ScoreboardTeamMap.put("Red", scoreboard.registerNewTeam("Red"));
+        ScoreboardTeamMap.put("Blue", scoreboard.registerNewTeam("Blue"));
+        ScoreboardTeamMap.put("Not Queued", scoreboard.registerNewTeam("Not Queued"));
+
+        ScoreboardTeamMap.get("Red").setPrefix(ChatColor.RED + "" + ChatColor.BOLD + "RED " + ChatColor.RESET);
+        ScoreboardTeamMap.get("Blue").setPrefix(ChatColor.AQUA + "" + ChatColor.BOLD + "BLUE " + ChatColor.RESET);
+        ScoreboardTeamMap.get("Not Queued").setPrefix(ChatColor.GRAY + "" + ChatColor.BOLD + "Not Queued " + ChatColor.RESET);
+
         TeamMap.put("Red", new KitTeam("Red", ChatColor.RED));
         TeamMap.put("Blue", new KitTeam("Blue", ChatColor.AQUA));
-        TeamMap.put("No Team", new KitTeam("No Team", ChatColor.GRAY));
+        TeamMap.put("Not Queued", new KitTeam("Not Queued", ChatColor.GRAY));
+
+        // scoreboard layout
+        HashMap<Integer, String> scoreboardLayout = new HashMap<>();
+        scoreboardLayout.put(13, new String(new char[15]).replace("\0", "▅"));
+        scoreboardLayout.put(12, "      ");
+        scoreboardLayout.put(11, "Author: " + ChatColor.GREEN + "benoolean");
+        scoreboardLayout.put(10, "    ");
+        scoreboardLayout.put(9, "   ");
+        scoreboardLayout.put(8, "Kills: " + ChatColor.GREEN);
+        scoreboardLayout.put(7, "Deaths: " + ChatColor.GREEN);
+        scoreboardLayout.put(6, "Score: " + ChatColor.GREEN);
+        scoreboardLayout.put(5, "  ");
+        scoreboardLayout.put(4, " ");
+        scoreboardLayout.put(3, ChatColor.GREEN + "Support me by following my");
+        scoreboardLayout.put(2, ChatColor.GREEN + "Github or visit my website.");
+        scoreboardLayout.put(1, ChatColor.GREEN + "More information at:");
+        scoreboardLayout.put(0, ChatColor.GOLD + "" + ChatColor.BOLD + "www.benoolean.com");
 
         objective.setDisplaySlot(DisplaySlot.SIDEBAR);
         objective.setDisplayName(ChatColor.YELLOW + "" + ChatColor.BOLD + "Game Information");
 
-        // team init
-        teamRed.setPrefix(ChatColor.RED + "" + ChatColor.BOLD + "RED " + ChatColor.RESET);
-        teamBlue.setPrefix(ChatColor.AQUA + "" + ChatColor.BOLD + "BLUE " + ChatColor.RESET);
-        teamNone.setPrefix(ChatColor.GRAY + "" + ChatColor.BOLD + "No Team " + ChatColor.RESET);
 
         // setting default empty scoreboard
-        for (int scoreboardIndex = 1; scoreboardIndex <= 14; scoreboardIndex++) {
-            Score score;
-
-            // first bar
-            if (scoreboardIndex == 14) {
-                score = objective.getScore(new String(new char[15]).replace("\0", "▅"));
-            }
-            else if (scoreboardIndex == 12) {
-                score = objective.getScore("Author: " + ChatColor.GREEN + "benoolean");
-            }
-            else if (scoreboardIndex == 10) {
-                score = objective.getScore("Kills: " + ChatColor.GREEN);
-            }
-            else if (scoreboardIndex == 9) {
-                score = objective.getScore("Deaths: " + ChatColor.GREEN);
-            }
-            else if (scoreboardIndex == 8) {
-                score = objective.getScore("Score: " + ChatColor.GREEN);
-            }
-            else if (scoreboardIndex == 4) {
-                score = objective.getScore(ChatColor.GREEN + "Support me by following my");
-            }
-            else if (scoreboardIndex == 3) {
-                score = objective.getScore(ChatColor.GREEN + "Github or visit my website.");
-            }
-            else if (scoreboardIndex == 2) {
-                score = objective.getScore(ChatColor.GREEN + "More information at:");
-            }
-            else if (scoreboardIndex == 1) {
-                score = objective.getScore(ChatColor.GOLD + "" + ChatColor.BOLD + "www.benoolean.com");
-            }
-            else {
-                score = objective.getScore(new String(new char[scoreboardIndex]).replace("\0", " "));
-            }
-
-            score.setScore(scoreboardIndex);
+        Set<Integer> lines = scoreboardLayout.keySet();
+        for (int line : lines) {
+            Score score = objective.getScore(scoreboardLayout.get(line));
+            score.setScore(line);
         }
     }
 
@@ -103,7 +89,28 @@ public class TeamScoreLogic implements Listener {
     @EventHandler
     public void PlayerJoinEvent(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        JoinTeam(player, "No Team");
+        if (player.teleport(new Location(player.getWorld(), 0.5F, 80F, 0.5F))) {
+            JoinTeam(player, "Not Queued");
+            return;
+        }
+
+        player.kickPlayer("Unable to spawn you in correctly!");
+    }
+
+    @EventHandler
+    public void PlayerMoveEvent(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        KitTeam kitTeam = PlayerTeamMap.get(player);
+
+        if (kitTeam == null) {
+            player.kickPlayer("Unable to queue into selected and default teams!");
+            return;
+        }
+
+        if (kitTeam.name == "Not Queued") {
+            player.sendMessage("Please select a team first by /team Blue or /team Red");
+            event.setCancelled(true);
+        }
     }
 
     public static void JoinTeam(Player player, String teamName) {
@@ -113,31 +120,37 @@ public class TeamScoreLogic implements Listener {
             PlayerKillCountMap.put(player, 0);
             player.setScoreboard(TeamScoreLogic.scoreboard);
 
-            if (teamName.equalsIgnoreCase("Red")) {
-                teamBlue.removeEntry(player.getName());
-                teamNone.removeEntry(player.getName());
-                teamRed.addEntry(player.getName());
-                player.setDisplayName(ChatColor.RED + "" + ChatColor.BOLD + "RED " + ChatColor.RESET + player.getName());
-            }
-            else if (teamName.equalsIgnoreCase("Blue")){
-                teamRed.removeEntry(player.getName());
-                teamNone.removeEntry(player.getName());
-                teamBlue.addEntry(player.getName());
-                player.setDisplayName(ChatColor.AQUA + "" + ChatColor.BOLD + "BLUE " + ChatColor.RESET + player.getName());
-            }
-            else {
-                teamRed.removeEntry(player.getName());
-                teamBlue.removeEntry(player.getName());
-                teamNone.addEntry(player.getName());
-                player.setDisplayName(ChatColor.GRAY + "" + ChatColor.BOLD + "No Team " + ChatColor.RESET + player.getName());
+            Set<String> ScoreboardTeamNames = ScoreboardTeamMap.keySet();
+            System.out.print(ScoreboardTeamNames);
+            Team team = ScoreboardTeamMap.get(teamName);
+            if (team == null) {
+                player.sendMessage(ChatColor.RED + "Error when choosing team. Team doesn't exist in HashMap");
+                return;
             }
 
-            player.sendMessage("You are now on team: " + teamName);
+            if (team.hasEntry(player.getName())) {
+                player.sendMessage(ChatColor.RED + "You are already on team " + team.getPrefix());
+                return;
+            }
 
+            team.addEntry(player.getName());
+            for (String ScoreboardTeamName : ScoreboardTeamNames) {
+                if (!ScoreboardTeamName.equalsIgnoreCase(teamName)) {
+                    Team teamNotEnrolled = ScoreboardTeamMap.get(ScoreboardTeamName);
+                    if (teamNotEnrolled != null) {
+                        teamNotEnrolled.removeEntry(player.getName());
+                    }
+                }
+            }
+
+            player.sendMessage("You are now on team: " + team.getPrefix());
+
+            // init player kill custom scores
             Team playerKillCount =  PlayerRegisterScore(player, "Kills");
             Team playerDeathCount =  PlayerRegisterScore(player, "Deaths");
             Team playerScore =  PlayerRegisterScore(player, "Score");
 
+            // setting player kill custom scores
             playerKillCount.addEntry("Kills: " + ChatColor.GREEN);
             playerKillCount.setSuffix("0");
             playerKillCount.setPrefix("");
@@ -150,8 +163,13 @@ public class TeamScoreLogic implements Listener {
             playerScore.setSuffix("0");
             playerScore.setPrefix("");
 
+            // attaching custom scores to player
             player.setScoreboard(TeamScoreLogic.scoreboard);
+
+            return;
         }
+
+        player.sendMessage(ChatColor.RED + "Team name not valid!");
     }
 
     /////////////////////////////////
